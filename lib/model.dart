71b9e115c3ce2a8292c21be25f3e6b933316fb3b
Maria Clara <mcfranca0107@gmail.com>
2022-05-02 17:03:54 -0300

    Algumas atividades - 02.05

--- lib/model.dart
+++ lib/model.dart
@@ -1,11 +1,37 @@
 
 enum Sexo {MASCULINO, FEMININO}
 
+Sexo? strToSexo(String? str) {
+  if(str == null){
+    return null;
+  }
+
+  switch(str){
+    case 'MASCULINO':
+    case 'M':
+      return Sexo.MASCULINO;
+    case 'FEMININO':
+    case 'F':
+      return Sexo.FEMININO;
+    default:
+      return null;
+  }
+}
+
 class Pessoa {
   String? nome;
   String? cpf;
   DateTime? nascimento;
   Sexo? sexo;
+
+  Pessoa(this.nome, this.cpf, this.nascimento, this.sexo);
+
+  Pessoa.fromMap(Map<String, String> map){
+    nome = map['nome'];
+    cpf = map['cpf'];
+    nascimento = DateTime.tryParse(map['nascimento'] ?? "2000-01-01");
+    sexo = strToSexo(map['sexo']);
+  }
 
   int? getIdade(){
 
@@ -28,6 +54,13 @@ class Pessoa {
 class Programador extends Pessoa{
   double? salario;
 
+  Programador(String? nome, String? cpf, DateTime? nascimento, Sexo? sexo, this.salario)
+      : super(nome, cpf, nascimento, sexo);
+
+  Programador.fromMap(Map<String, String> map) : super.fromMap(map){
+    salario = double.tryParse(map['salario'] ?? "0.0");
+  }
+
   @override
   String toString() {
     return "{Nome: '$nome'. CPF: '$cpf'. Nascimento: '$nascimento'. Idade: '${getIdade()}'. Sexo: '$sexo'. Salário: '$salario'.}";
@@ -36,21 +69,29 @@ class Programador extends Pessoa{
 }
 
 void mainEntidades(){
-  Pessoa pessoa = Pessoa();
 
-  pessoa.nome = "Maria Clara Franca";
-  pessoa.cpf = "127.512.529-80";
-  pessoa.nascimento = DateTime(2002, 7, 1);
-  pessoa.sexo = Sexo.FEMININO;
+  final map = {
+    'nome': 'Maria',
+    'nascimento': '2002-07-01',
+    'sexo': 'F',
+    'salario': '10000.0'
+  };
 
-  Programador prog = Programador();
+  Pessoa pessoa = Programador.fromMap(map);
 
-  prog.nome = "Ana Rita Franca";
-  prog.cpf = "123.456.789-10";
-  prog.nascimento = DateTime(2005, 6, 17);
-  prog.sexo = Sexo.FEMININO;
-  prog.salario = 10000.00;
+  // pessoa.nome = "Maria Clara Franca";
+  // pessoa.cpf = "127.512.529-80";
+  // pessoa.nascimento = DateTime(2002, 7, 1);
+  // pessoa.sexo = Sexo.FEMININO;
+
+  // Programador prog = Programador();
+
+  // prog.nome = "Ana Rita Franca";
+  // prog.cpf = "123.456.789-10";
+  // prog.nascimento = DateTime(2005, 6, 17);
+  // prog.sexo = Sexo.FEMININO;
+  // prog.salario = 10000.00;
 
   print(pessoa.toString());
-  print(prog.toString());
+  // print(prog.toString());
 }
